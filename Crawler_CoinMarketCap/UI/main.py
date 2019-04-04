933f4fe759f7e3cefb2fe4dc91014f5886e0075a
YCPan-tw <r0554628@g.ncu.edu.tw>
2019-04-05 00:47:35 +0800

    UI_version

--- Crawler_CoinMarketCap/UI/main.py
+++ Crawler_CoinMarketCap/UI/main.py
@@ -10,6 +10,7 @@ class MyDlg(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         # new method
+        #https://codertw.com/%E7%A8%8B%E5%BC%8F%E8%AA%9E%E8%A8%80/433908/
         self.ui.buttonBox.accepted.connect(lambda: self.onButtonClick(self.ui.lineEdit.text(),self.ui.lineEdit_2.text(),self.ui.lineEdit_3.text()))
 
     def onButtonClick(self,crypto_name,sdate,edate):
